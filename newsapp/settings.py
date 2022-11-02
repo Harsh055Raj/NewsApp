94b701af9d4c37a241e274f69b3b1536a1daa6a2
SANCHIT AGARWAL <68566546+sanchit781@users.noreply.github.com>
2022-11-02 23:12:08 +0530

    CSRF

--- newsapp/settings.py
+++ newsapp/settings.py
@@ -133,6 +133,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+CSRF_TRUSTED_ORIGINS = ['https://*.dailyshorts.in']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
